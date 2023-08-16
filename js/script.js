9496c5dbbe91ed37bb0936f73cb2fa8a6d72f20c
Abdias <lagraceparle98@gmail.com>
2023-08-16 16:36:27 +0100

    push

--- js/script.js
+++ js/script.js
@@ -22,18 +22,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
       if (response.ok) {
         messageParagraph.textContent = "Abonnement à la newsletter réussie !";
+        messageParagraph.classList.remove("error");
+        messageParagraph.classList.add("success");
         emailInput.value = "";
       } else {
         const errorData = await response.json();
         console.error("Erreur de souscription:", errorData.message);
         messageParagraph.textContent =
           "Erreur de souscription: " + errorData.message;
+        messageParagraph.classList.remove("success");
+        messageParagraph.classList.add("error");
       }
       console.log(response);
     } catch (error) {
       console.error("Erreur lors de la requête:", error);
       messageParagraph.textContent =
         "Erreur lors de la requête. Veuillez réessayer plus tard.";
+      messageParagraph.classList.remove("success");
+      messageParagraph.classList.add("error");
     }
   });
 });
