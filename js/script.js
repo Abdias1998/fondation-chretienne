16a79bbad2335299fc006bb311d1a473077b1381
Abdias <lagraceparle98@gmail.com>
2023-08-18 12:41:43 +0100

    Commit for wednesday

--- js/script.js
+++ js/script.js
@@ -1,20 +1,61 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const subscribeForm = document.getElementById("subscribeForm");
+//   const messageParagraph = document.getElementById("messageParagraph");
+
+//   subscribeForm.addEventListener("submit", async (event) => {
+//     event.preventDefault();
+
+//     const emailInput = document.getElementById("emails");
+//     const email = emailInput.value;
+
+//     try {
+//       const response = await fetch(
+//         "https://backend-lagraceparle.onrender.com/api/v1/subscribe-donateur",
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify({ email }),
+//         }
+//       );
+
+//       if (response.ok) {
+//         messageParagraph.textContent = "Abonnement à la newsletter réussie !";
+//         messageParagraph.classList.remove("error");
+//         messageParagraph.classList.add("success");
+//         emailInput.value = "";
+//       } else {
+//         const errorData = await response.json();
+//         console.error("Erreur de souscription:", errorData.message);
+//         messageParagraph.textContent =
+//           "Erreur de souscription: " + errorData.message;
+//         messageParagraph.classList.remove("success");
+//         messageParagraph.classList.add("error");
+//       }
+//       console.log(response);
+//     } catch (error) {
+//       console.error("Erreur lors de la requête:", error);
+//       messageParagraph.textContent =
+//         "Erreur lors de la requête. Veuillez réessayer plus tard.";
+//       messageParagraph.classList.remove("success");
+//       messageParagraph.classList.add("error");
+//     }
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
   const subscribeForm = document.getElementById("subscribeForm");
   const messageParagraph = document.getElementById("messageParagraph");
-  const submitButton = document.querySelector('button[type="submit"]');
 
   subscribeForm.addEventListener("submit", async (event) => {
     event.preventDefault();
-
-    // Désactiver le formulaire et le bouton de soumission
-    subscribeForm.classList.add("disabled");
-    submitButton.disabled = true;
 
     const emailInput = document.getElementById("emails");
     const email = emailInput.value;
 
     // Récupérer les valeurs des champs "name", "audio" et "video"
-    const nameInput = document.getElementById("name");
+    const nameInput = document.getElementById("names");
     const names = nameInput.value;
 
     const audioSelect = document.getElementById("audio");
@@ -41,13 +82,9 @@ document.addEventListener("DOMContentLoaded", () => {
         messageParagraph.classList.add("success");
         emailInput.value = "";
         nameInput.value = ""; // Réinitialiser la valeur du champ "name"
-        audioSelect.value = ""; // Réinitialiser la valeur du champ "name"
-        videoSelect.value = ""; // Réinitialiser la valeur du champ "name"
 
-        // Réactiver le formulaire et le bouton de soumission après 3 secondes (3000 ms)
+        // Efface le message après 3 secondes (3000 ms)
         setTimeout(() => {
-          subscribeForm.classList.remove("disabled");
-          submitButton.disabled = false;
           messageParagraph.textContent = "";
         }, 3000);
       } else {
@@ -57,10 +94,6 @@ document.addEventListener("DOMContentLoaded", () => {
           "Erreur de souscription: " + errorData.message;
         messageParagraph.classList.remove("success");
         messageParagraph.classList.add("error");
-
-        // Réactiver le formulaire et le bouton de soumission
-        subscribeForm.classList.remove("disabled");
-        submitButton.disabled = false;
       }
     } catch (error) {
       console.error("Erreur lors de la requête:", error);
@@ -68,10 +101,6 @@ document.addEventListener("DOMContentLoaded", () => {
         "Erreur lors de la requête. Veuillez réessayer plus tard." * error;
       messageParagraph.classList.remove("success");
       messageParagraph.classList.add("error");
-
-      // Réactiver le formulaire et le bouton de soumission
-      subscribeForm.classList.remove("disabled");
-      submitButton.disabled = false;
     }
   });
 });
