6701e4337c52c0216e65e0eb0edff87b224d308e
Abdias <lagraceparle98@gmail.com>
2023-08-18 13:22:51 +0100

    Commit for wednesday

--- js/script.js
+++ js/script.js
@@ -1,52 +1,7 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const subscribeForm = document.getElementById("subscribeForm");
-//   const messageParagraph = document.getElementById("messageParagraph");
-
-//   subscribeForm.addEventListener("submit", async (event) => {
-//     event.preventDefault();
-
-//     const emailInput = document.getElementById("emails");
-//     const email = emailInput.value;
-
-//     try {
-//       const response = await fetch(
-//         "https://backend-lagraceparle.onrender.com/api/v1/subscribe-donateur",
-//         {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify({ email }),
-//         }
-//       );
-
-//       if (response.ok) {
-//         messageParagraph.textContent = "Abonnement à la newsletter réussie !";
-//         messageParagraph.classList.remove("error");
-//         messageParagraph.classList.add("success");
-//         emailInput.value = "";
-//       } else {
-//         const errorData = await response.json();
-//         console.error("Erreur de souscription:", errorData.message);
-//         messageParagraph.textContent =
-//           "Erreur de souscription: " + errorData.message;
-//         messageParagraph.classList.remove("success");
-//         messageParagraph.classList.add("error");
-//       }
-//       console.log(response);
-//     } catch (error) {
-//       console.error("Erreur lors de la requête:", error);
-//       messageParagraph.textContent =
-//         "Erreur lors de la requête. Veuillez réessayer plus tard.";
-//       messageParagraph.classList.remove("success");
-//       messageParagraph.classList.add("error");
-//     }
-//   });
-// });
-
 document.addEventListener("DOMContentLoaded", () => {
   const subscribeForm = document.getElementById("subscribeForm");
   const messageParagraph = document.getElementById("messageParagraph");
+  const text = document.getElementById("text");
 
   subscribeForm.addEventListener("submit", async (event) => {
     event.preventDefault();
@@ -54,7 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const emailInput = document.getElementById("emails");
     const email = emailInput.value;
 
-    // Récupérer les valeurs des champs "name", "audio" et "video"
     const nameInput = document.getElementById("names");
     const names = nameInput.value;
 
@@ -81,12 +35,14 @@ document.addEventListener("DOMContentLoaded", () => {
         messageParagraph.classList.remove("error");
         messageParagraph.classList.add("success");
         emailInput.value = "";
-        nameInput.value = ""; // Réinitialiser la valeur du champ "name"
+        nameInput.value = "";
+        subscribeForm.style.display = "none";
+        text.innerHTML =
+          "<p>Merci de votre inscription à la Pensée La Grace Parle. Pour activer votre abonnement gratuit, la loi RGPD nous oblige à présent à vous demander de confirmer votre abonnement. Pour cela, il vous suffit de cliquer sur le lien suivant :</p>";
 
-        // Efface le message après 3 secondes (3000 ms)
         setTimeout(() => {
           messageParagraph.textContent = "";
-        }, 3000);
+        }, 5000);
       } else {
         const errorData = await response.json();
         console.error("Erreur de souscription:", errorData.message);
@@ -98,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
     } catch (error) {
       console.error("Erreur lors de la requête:", error);
       messageParagraph.textContent =
-        "Erreur lors de la requête. Veuillez réessayer plus tard." * error;
+        "Erreur lors de la requête. Veuillez réessayer plus tard. " + error;
       messageParagraph.classList.remove("success");
       messageParagraph.classList.add("error");
     }
