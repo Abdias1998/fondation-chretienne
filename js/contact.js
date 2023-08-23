9c60ad24e4db914711c8d5e677a522373cfedf7e
Abdias <lagraceparle98@gmail.com>
2023-08-23 09:35:09 +0100

    Commit automatisé

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,16 @@
+const whatsappCheckbox = document.getElementById("whatsappCheckbox");
+const phoneNumberField = document.getElementById("phoneNumberField");
+const phoneNumberInput = document.getElementById("phoneNumber");
+
+whatsappCheckbox.addEventListener("change", function () {
+  if (whatsappCheckbox.checked) {
+    phoneNumberField.style.display = "block";
+    phoneNumberInput.required = true;
+  } else {
+    phoneNumberField.style.display = "none";
+    phoneNumberInput.required = false;
+  }
+});
 document.addEventListener("DOMContentLoaded", () => {
   const subscribeForm = document.getElementById("subscribeForm");
   const messageParagraph = document.getElementById("messageParagraph");
