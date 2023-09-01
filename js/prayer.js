08e10b894f03b46096a664f7ac2c6db64ce52c13
Abdias <lagraceparle98@gmail.com>
2023-09-01 13:24:42 +0100

    Commit automatisé

--- js/prayer.js
+++ js/prayer.js
@@ -11,3 +11,96 @@ whatsappCheckbox.addEventListener("change", function () {
     phoneNumberInput.required = false;
   }
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const subscribeForm = document.getElementById("subscribeForm");
+  const messageParagraph = document.getElementById("messageParagraph");
+
+  const submit = document.getElementById("submitButton");
+  const ancienTexteBouton = submit.textContent; // Sauvegarde du texte original du bouton
+
+  subscribeForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    // Mettre à jour le texte du bouton pendant la requête
+    submit.textContent = "Envoi en cours...";
+    submit.disabled = true; // Désactiver le bouton pendant la requête
+
+    const emailInput = document.getElementById("emails");
+    const email = emailInput.value;
+    const sexeInput = document.getElementById("sexe");
+    const sexe = sexeInput.value;
+    const paysInput = document.getElementById("pays");
+    const pays = paysInput.value;
+
+    const nameInput = document.getElementById("names");
+    const names = nameInput.value;
+
+    const subjectSelect = document.getElementById("subject");
+    const subject = subjectSelect.value;
+
+    const messageSelect = document.getElementById("message");
+    const message = messageSelect.value;
+
+    const tel = phoneNumberInput.value;
+
+    try {
+      const response = await fetch(
+        "https://backend-lagraceparle.onrender.com/api/v2/prayer",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            names,
+            subject,
+            message,
+            tel,
+            sexe,
+            pays,
+          }),
+        }
+      );
+
+      if (response.ok) {
+        messageParagraph.textContent = `
+        Nous vous remercions sincèrement d'avoir partagé votre demande de prière avec nous. Nous tenons à vous informer que nous avons bien reçu votre demande et que nous allons la prendre en compte dans nos prières.
+        
+        La foi est une force puissante, et nous croyons que Dieu entend nos prières et répond à nos besoins selon Sa volonté. Nous vous encourageons à garder la foi et à croire que Dieu est à l'œuvre dans votre vie. Même lorsque les réponses ne sont pas immédiates, continuez à prier et à avoir confiance en Sa bonté et en Sa sagesse.
+        
+        Nous sommes là pour vous soutenir dans cette période de prière et d'attente. Si vous avez d'autres besoins spirituels ou des questions, n'hésitez pas à nous contacter à tout moment.
+        
+        Que la paix de Dieu qui dépasse toute compréhension vous remplisse et que Sa grâce abonde dans votre vie.`;
+        messageParagraph.classList.remove("error");
+        messageParagraph.classList.add("success");
+        emailInput.value = "";
+        nameInput.value = "";
+        subjectSelect.value = "";
+        phoneNumberInput.value = "";
+        messageSelect.value = "";
+        sexeInput.value = "";
+        paysInput.value = "";
+
+        setTimeout(() => {
+          messageParagraph.textContent = "";
+        }, 40000);
+      } else {
+        const errorData = await response.json();
+        console.error("Erreur de souscription:", errorData.message);
+        messageParagraph.textContent = errorData.message;
+        messageParagraph.classList.remove("success");
+        messageParagraph.classList.add("error");
+      }
+    } catch (error) {
+      console.error("Erreur lors de la requête:", error);
+      messageParagraph.textContent = error;
+      messageParagraph.classList.remove("success");
+      messageParagraph.classList.add("error");
+    } finally {
+      // Réactiver le bouton et rétablir le texte original
+      submit.disabled = false;
+      submit.textContent = ancienTexteBouton;
+    }
+  });
+});
