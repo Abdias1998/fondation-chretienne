8cd50defa6b5c0fab2691ec9f7b5858e3f670461
Abdias <lagraceparle98@gmail.com>
2023-09-13 20:00:36 +0100

    Commit automatisé

--- js/main.js
+++ js/main.js
@@ -1,83 +1,105 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
 
+  // Initiate the wowjs
+  new WOW().init();
 
-    // Fixed Navbar
-    $(window).scroll(function () {
-        if ($(window).width() < 992) {
-            if ($(this).scrollTop() > 45) {
-                $('.fixed-top').addClass('bg-dark shadow');
-            } else {
-                $('.fixed-top').removeClass('bg-dark shadow');
-            }
-        } else {
-            if ($(this).scrollTop() > 45) {
-                $('.fixed-top').addClass('bg-dark shadow').css('top', -45);
-            } else {
-                $('.fixed-top').removeClass('bg-dark shadow').css('top', 0);
-            }
-        }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+  // Fixed Navbar
+  $(window).scroll(function () {
+    if ($(window).width() < 992) {
+      if ($(this).scrollTop() > 45) {
+        $(".fixed-top").addClass("bg-dark shadow");
+      } else {
+        $(".fixed-top").removeClass("bg-dark shadow");
+      }
+    } else {
+      if ($(this).scrollTop() > 45) {
+        $(".fixed-top").addClass("bg-dark shadow").css("top", -45);
+      } else {
+        $(".fixed-top").removeClass("bg-dark shadow").css("top", 0);
+      }
+    }
+  });
 
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 
-    // Causes progress
-    $('.causes-progress').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
+  // Causes progress
+  $(".causes-progress").waypoint(
+    function () {
+      $(".progress .progress-bar").each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + "%");
+      });
+    },
+    { offset: "80%" }
+  );
 
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1000,
-        center: true,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            }
-        }
-    });
-
-    
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: false,
+    smartSpeed: 1000,
+    center: true,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+    },
+  });
 })(jQuery);
 
+// Fonction pour vérifier s'il est lundi entre 22h30 et minuit et gérer l'icône
+function checkAndHandleIcon() {
+  const now = new Date();
+  const liveIcon = document.getElementById("live-icon");
+
+  if (
+    now.getDay() === 3 &&
+    now.getHours() === 19 &&
+    now.getMinutes() >= 30 &&
+    now.getHours() < 24
+  ) {
+    // C'est un lundi entre 22h30 et minuit, afficher l'icône
+    liveIcon.style.display = "block";
+    document.getElementById("live-icon").classList.add("blinking");
+  } else {
+    // Sinon, cacher l'icône
+    liveIcon.style.display = "none";
+    document.getElementById("live-icon").classList.remove("blinking");
+  }
+}
+
+// Appeler la fonction de vérification toutes les minutes
+setInterval(checkAndHandleIcon, 60000);
+
+// Appeler la fonction initiale
+checkAndHandleIcon();
