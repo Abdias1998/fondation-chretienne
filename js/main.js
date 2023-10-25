23a9d2efcc59969b9ff1171183123846008bdf29
Abdias1998 <lagraceparle98@gmail.com>
2023-10-25 15:38:40 +0100

    Commit automatisé

--- js/main.js
+++ js/main.js
@@ -125,3 +125,20 @@ window.addEventListener("click", function (event) {
     closeModal();
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("myModal");
+  const closeBtn = document.getElementById("close-btn");
+
+  modal.style.display = "block";
+
+  closeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+});
